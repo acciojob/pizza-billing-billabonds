3c01df6a72e19ea722b66ee0d6e867d4d1751f67
Shivam <goelshivam2012@gmail.com>
2023-03-16 00:12:19 +0530

    Assignment done

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,10 +5,21 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    private Boolean isCheeseAdded;
+    private Boolean isToppingAdded;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+
+        if(isVeg){
+            System.out.println("Base price of the Pizza: 300");
+            price += 300;
+        }
+        else{
+            System.out.println("Base price of the Pizza: 400");
+            price += 400;
+        }
     }
 
     public int getPrice(){
@@ -17,14 +28,31 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(isCheeseAdded == false) {
+            System.out.println("Extra Cheese Added: 80");
+            isCheeseAdded = true;
+            price += 80;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(isToppingAdded == false) {
+            if (isVeg) {
+                System.out.println("Extra Toppings Added: 70");
+                price += 70;
+            } else {
+                System.out.println("Extra Toppings Added: 120");
+                price += 120;
+            }
+            isToppingAdded = true;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        System.out.println("Paperbag Added: 20");
+        price += 20;
     }
 
     public String getBill(){
